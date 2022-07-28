6343a76cb108c6fd91b4e78594dbd8accdc87a57
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-28 09:40:39 -0300

    feat: veihcle model done

---
+++ src/models/Vehicle.ts
@@ -0,0 +1,54 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("vehicles")
+class Vehicle {
+  @PrimaryGeneratedColumn("uuid")
+  readonly id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  type_of_ad: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  km: number;
+
+  @Column({ type: "decimal", precision: 20, scale: 2 })
+  price: number;
+
+  @Column({ length: 500 })
+  description: string;
+
+  @Column()
+  type_of_veihcle: string;
+
+  @Column()
+  cover_image: string;
+
+  @Column()
+  gallery_image: string;
+
+  @Column({ nullable: true })
+  gallery_image2: string;
+
+  @Column({ nullable: true })
+  gallery_image3: string;
+
+  @Column({ nullable: true })
+  gallery_image4: string;
+
+  @Column({ nullable: true })
+  gallery_image5: string;
+
+  @Column({ nullable: true })
+  gallery_image6: string;
+
+  @Column({ default: true })
+  published: boolean;
+}
+
+export default Vehicle
