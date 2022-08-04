93b0d5a4b199f88162d9f09d7ed4cc1e3887116c
Alexandre Araujo <alexandre01393@gmail.com>
2022-08-04 12:03:40 -0300

    fix: fixing create vehicle service

--- src/services/vehicles/createVehicle.service.ts
+++ src/services/vehicles/createVehicle.service.ts
@@ -11,27 +11,37 @@ interface VehicleProps {
   type_of_vehicle: string;
   cover_image: string;
   gallery_image: string;
+  gallery_image2?: string;
+  gallery_image3?: string;
+  gallery_image4?: string;
+  gallery_image5?: string;
+  gallery_image6?: string;
 }
 
 const createVehicleService = async (data: VehicleProps) => {
-    const vehicleRepository = AppDataSource.getRepository(Vehicle)
+  const vehicleRepository = AppDataSource.getRepository(Vehicle);
 
-    const newVehicle = new Vehicle()
-    newVehicle.title = data.title
-    newVehicle.type_of_ad = data.type_of_ad
-    newVehicle.year = data.year
-    newVehicle.km = data.km
-    newVehicle.price = data.price
-    newVehicle.description = data.description
-    newVehicle.type_of_vehicle = data.type_of_vehicle
-    newVehicle.cover_image = data.cover_image
-    newVehicle.gallery_image = data.gallery_image
+  const newVehicle = new Vehicle();
+  newVehicle.title = data.title;
+  newVehicle.type_of_ad = data.type_of_ad;
+  newVehicle.year = data.year;
+  newVehicle.km = data.km;
+  newVehicle.price = data.price;
+  newVehicle.description = data.description;
+  newVehicle.type_of_vehicle = data.type_of_vehicle;
+  newVehicle.cover_image = data.cover_image;
+  newVehicle.gallery_image = data.gallery_image;
+  newVehicle.gallery_image2 = data.gallery_image2!;
+  newVehicle.gallery_image3 = data.gallery_image3!;
+  newVehicle.gallery_image4 = data.gallery_image4!;
+  newVehicle.gallery_image5 = data.gallery_image5!;
+  newVehicle.gallery_image6 = data.gallery_image6!;
 
-    vehicleRepository.create(newVehicle)
+  vehicleRepository.create(newVehicle);
 
-    await vehicleRepository.save(newVehicle)
+  await vehicleRepository.save(newVehicle);
 
-    return newVehicle
+  return newVehicle;
 };
 
-export default createVehicleService
+export default createVehicleService;
