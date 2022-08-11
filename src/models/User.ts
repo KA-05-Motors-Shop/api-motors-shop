f276c823aaecefbf0bc243eeb2a5f69d1925a92e
Luiz Antonio Motta <mottaluizantonio@gmail.com>
2022-08-11 10:28:05 -0300

    feat: user model

---
+++ src/models/User.ts
@@ -0,0 +1,54 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("users")
+class User {
+  @PrimaryGeneratedColumn("uuid")
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  cpf: number;
+
+  @Column()
+  cel: number;
+
+  @Column()
+  birth_date: number;
+
+  @Column({ length: 500 })
+  description: string;
+
+  @Column()
+  cep: number;
+
+  @Column()
+  state: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  street: string;
+
+  @Column()
+  number: number;
+
+  @Column()
+  complement: string;
+
+  @Column()
+  account_type: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  confirm_password: string;
+}
+
+export default User;
