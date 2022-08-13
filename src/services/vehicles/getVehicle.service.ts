77ce77a5b64fb95a469def764a70447ad4c79f38
Alexandre Araujo <alexandre01393@gmail.com>
2022-08-13 13:55:23 -0300

    feat: query builder implemented

--- src/services/vehicles/getVehicle.service.ts
+++ src/services/vehicles/getVehicle.service.ts
@@ -3,7 +3,11 @@ import Vehicle from "../../models/Vehicle";
 
 const listVehiclesService = async () => {
   const vehiclesRepository = AppDataSource.getRepository(Vehicle);
-  const vehicles = await vehiclesRepository.find();
+  const vehicles = await vehiclesRepository
+    .createQueryBuilder("vehicles")
+    .leftJoinAndSelect("vehicles.user", "users")
+    .where("vehicles.userId = users.id")
+    .getMany();
 
   const availableVehicles = vehicles.filter(({ published }) => published);
 
